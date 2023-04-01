8afab048b6881e351a7eb9ba93d48621981f86bc
Carlos Santos <carsantir@gmail.com>
2023-04-01 17:29:21 +0200

    Subida de proyecto base de SpringBoot

---
+++ PruebaTecnicaMercadona/src/test/java/com/mercadona/tests/PruebaTecnicaMercadonaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaMercadonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
